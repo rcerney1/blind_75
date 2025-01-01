6d33a8777b89c5b5510d8ec0601eea21260e8be7
rcerney <ryancerney1@gmail.com>
2025-01-01 13:47:42 -0800

    two_pointers

---
+++ problems/Two_pointers/pair_with_target_sum.js
@@ -0,0 +1,5 @@
+// Given an array of numbers sorted in ascending order and a target sum
+// find a pair in the array whose sum is equal to the given target.
+
+// Write a function to return the indices of the two numbers (i.e. the pair) 
+// such that they add up to the given target. If no such pair exists return [-1, -1]
