6825482ce99d68d6bc7f9c61e0cffb12d9941061
rcerney <ryancerney1@gmail.com>
2025-01-01 13:49:27 -0800

    eh

--- problems/Two_pointers/pair_with_target_sum.js
+++ problems/Two_pointers/pair_with_target_sum.js
@@ -2,4 +2,4 @@
 // find a pair in the array whose sum is equal to the given target.
 
 // Write a function to return the indices of the two numbers (i.e. the pair) 
-// such that they add up to the given target. If no such pair exists return [-1, -1]
+// such that they add up to the given target. If no such pair exists return [-1, -1] dd
