ca5f78b7f69cb36ac5b35790e77ff8dafd7117a1
rcerney <ryancerney1@gmail.com>
2025-01-21 12:08:16 -0800

    stack

---
+++ dsa/stack_func.js
@@ -0,0 +1,34 @@
+const Stack = function() {
+    this.count = 0;
+    this.storage = {};
+
+    this.push = function(value) {
+        this.storage[this.count] = value;
+        this.count++;
+    }
+
+    this.pop = function() {
+        if (this.count === 0) {
+            return undefined;
+        }
+
+        this.count--;
+        let result = this.storage[this.count];
+        delete this.storage[this.count];
+        return result;
+    }
+
+    this.size = function() {
+        return this.count;
+    }
+
+    this.peek = function() {
+        return this.storage[this.count - 1]
+    }
+}
+
+const myStack = new Stack();
+
+myStack.push(1)
+myStack.push(2)
+console.log(myStack.count)
