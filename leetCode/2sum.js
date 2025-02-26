0922869966d8260539d975b73173545e91ca6ef3
rcerney <ryancerney1@gmail.com>
2025-02-25 16:54:34 -0800

    2sum comments

--- leetCode/2sum.js
+++ leetCode/2sum.js
@@ -8,9 +8,22 @@ const twoSum = (nums, target) => {
             return [i, map.get(compliment)]
         } else {
             map.set(nums[i], i)
-            
+            console.log(map) 
         }
     }
+
+    return false;
 }
 
-console.log(twoSum([2,7,11,15], 9))
+console.log(twoSum([2,6,11,15], 9))
+
+
+//create a new map
+//create a loop through the numbers array
+//each loop make a compliment variable
+    //the compliment will be the target - numbers[i]
+//if the map has the compliment variable
+    //return [i, map.get(compliment)]
+    //else, set map to [nums[i], i]
+//end loop
+//return false since we went through the loop with no returns
