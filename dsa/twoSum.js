c02fbd6f0e85a132377541d41fedda0386248666
rcerney <ryancerney1@gmail.com>
2025-04-18 17:50:22 -0700

    two sum leet code problem

---
+++ dsa/twoSum.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    const map = new Map();
+
+    for (let i = 0; i < nums.length; i++) {
+        const complement = target - nums[i];
+
+        if (map.has(complement)) {
+            return [map.get(complement), i];
+        }
+
+        map.set(nums[i], i);
+    }
+
+    return []; // technically won't happen based on constraints
+};
+
+console.log(twoSum([2, 7, 11, 15], 9)); // Output: [0, 1]
