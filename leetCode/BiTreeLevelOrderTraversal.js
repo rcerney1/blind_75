454ebafb8a299e185942a0a23272d179ca2988e7
rcerney <ryancerney1@gmail.com>
2025-05-22 20:45:16 -0700

    binary tree level order traversal

---
+++ leetCode/BiTreeLevelOrderTraversal.js
@@ -0,0 +1,45 @@
+
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var levelOrder = function(root) {
+    if (!root) return [];
+
+    let result = [];
+    let queue = [root];
+
+    while (queue.length > 0) {
+        let levelSize = queue.length;
+        let currentLevel = [];
+
+        for (let i = 0; i < levelSize; i++) {
+            let node = queue.shift();
+            currentLevel.push(node.val);
+            if (node.left) queue.push(node.left);
+            if (node.right) queue.push(node.right);
+        }
+
+        result.push(currentLevel);
+    }
+
+    return result;
+};
+
+// Example test case
+function TreeNode(val, left = null, right = null) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+}
+
+const root = new TreeNode(3,
+    new TreeNode(9),
+    new TreeNode(20,
+        new TreeNode(15),
+        new TreeNode(7)
+    )
+);
+
+console.log(levelOrder(root));
+// Output: [[3], [9, 20], [15, 7]]
